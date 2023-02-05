c132940db373b80b2482ff0a6509185550a6207c
LyudmilaDr <110781702+LyudmilaDr@users.noreply.github.com>
2023-02-05 19:24:00 +0000

    Deploying to gh-pages from @ LyudmilaDr/goit-js-hw-09@08cb93ffb5e87eb5efa8117cb7faf9803ddacdba 🚀

--- 01-color-switcher.66d59667.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(d)})),t.disabled=!0;
-//# sourceMappingURL=01-color-switcher.66d59667.js.map
