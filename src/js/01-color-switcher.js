d617cea2efd8f353d289c6f6a21f4531a3bbdadf
LyudmilaDr <ludralo44@gmail.com>
2023-02-05 21:16:54 +0200

    pushfile2

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+let intervalId;
+btnStop.disabled = true;
+
+function onClickStart() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+    }
+
+function onClickStop() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+   clearInterval(intervalId);
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
